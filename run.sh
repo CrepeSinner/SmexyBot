7fd6071345ebbbd8e7916f2777c0178616195563
wisdomchuck <12144116+wisdomchuck@users.noreply.github.com>
2020-06-01 17:32:44 -0400

    fixes for SwexyBot

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-python3.5 -V > /dev/null 2>&1 || {
-	echo >&2 "Python 3.5 doesn't seem to be installed! Do you have a weird installation?"
+python3.6 -V > /dev/null 2>&1 || {
+	echo >&2 "Python 3.6 doesn't seem to be installed! Do you have a weird installation?"
 	echo >&2 "If you have python 3.5, use it to run bot.py instead of this script."
 	exit 1; }
 
-python3.5 bot.py
+# Heroku config var handling...
+sed "s/Token =/Token = $token/" "s/Owner_ID =/Owner_ID = $owner" "s/Developer_IDs =/Developer_IDs = $developer" config/config.ini > config/config.ini.new
+mv config/config.ini.new config/config.ini
+
+python3.6 bot.py
